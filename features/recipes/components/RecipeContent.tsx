6aa99aa05fffb15c9c0048c0bd71ae0d14fc2d77
Samuel Dolezal <samko.dolezal@gmail.com>
2026-02-05 18:03:39 +0100

    small changes

--- features/recipes/components/RecipeContent.tsx
+++ features/recipes/components/RecipeContent.tsx
@@ -1,32 +1,11 @@
 "use client";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import MealsSearched from "./MealsSearched";
 
 const RecipeContent = () => {
-  const [input, setInput] = useState("");
-  const [cuisine, setCuisine] = useState("");
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const callBackFunc = () => {
-      const query = searchParams.get("q");
-      if (query) {
-        setInput(query);
-      }
-    };
-    callBackFunc();
-  }, [searchParams]);
-
   return (
     <div className="w-full h-full">
-      <SearchBar
-        input={input}
-        setInput={setInput}
-        cuisine={cuisine}
-        setCuisine={setCuisine}
-      />
+      <SearchBar />
       <MealsSearched />
     </div>
   );
