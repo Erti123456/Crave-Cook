99c8677a5541fb16db255bfac52985199b4f5c39
Samuel Dolezal <samko.dolezal@gmail.com>
2026-03-18 19:45:25 +0100

    prisma connection

--- lib/getRecipeById.ts
+++ lib/getRecipeById.ts
@@ -5,7 +5,7 @@ import apiClient from "./axios";
 async function getRecipeById(id: string): Promise<Recipe | undefined> {
   try {
     const res = await apiClient.get<Recipe | null>(
-      `recipes/${id}/information?includeNutrition=true`,
+      `/recipes/${id}/information?includeNutrition=true`,
     );
     return res.data ?? undefined;
   } catch (error) {
