830bfbcad19afea4ea58ffb6fc3518a221912377
Samuel Dolezal <samko.dolezal@gmail.com>
2026-04-10 17:12:49 +0200

    fix: bug

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -3,6 +3,7 @@ import prisma from "@/lib/prisma";
 import { redirect } from "next/navigation";
 import getRecipeById from "@/lib/getRecipeById";
 import MealCard from "@/features/recipes/components/listing/MealCard";
+import type { Favorite } from "@prisma/client";
 
 const Page = async () => {
   const { userId } = await auth();
@@ -15,7 +16,7 @@ const Page = async () => {
     where: { userId },
   });
 
-  const recipePromises = favorites.map((f) => getRecipeById(f.recipeId));
+  const recipePromises = favorites.map((f: Favorite) => getRecipeById(f.recipeId));
   const recipes = (await Promise.all(recipePromises)).filter(Boolean);
 
   return (
