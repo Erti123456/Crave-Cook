3444765b99b6263cce53dd8e5b69197d63e24a10
Samuel Dolezal <samko.dolezal@gmail.com>
2026-04-12 16:18:59 +0200

    commit

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 const Page = () => {
   return (
     <div 
-      className="fixed inset-0 flex justify-center items-center bg-cover bg-center"
+      className="min-h-[calc(100vh-18rem)] flex justify-center items-center bg-cover bg-center px-4 py-32"
       style={{ backgroundImage: "url('/high-quality-food.jpg')" }}
     >
       <div className="bg-white/80 p-8 rounded-xl shadow-lg backdrop-blur-sm">
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 const Page = () => {
   return (
     <div 
-      className="fixed inset-0 flex items-center justify-center bg-cover bg-center"
+      className="min-h-[calc(100vh-18rem)] flex items-center justify-center bg-cover bg-center px-4 py-32"
       style={{ backgroundImage: "url('/high-quality-food.jpg')" }}
     >
       <div className="bg-white/80 p-8 rounded-xl shadow-lg backdrop-blur-sm">
